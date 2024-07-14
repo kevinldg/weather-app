01ee17fdb3444a2b458e372acef67674b8a3c338
GuNShOtzZ <kevinloeding@outlook.de>
2024-07-15 00:41:23 +0200

    Show weather of one city

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,30 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 export default function Home() {
-  return (
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const currentWeatherURL =
+      "https://api.open-meteo.com/v1/forecast?latitude=52.5244&longitude=13.4105&current=temperature_2m&timezone=Europe%2FBerlin&forecast_days=1&models=icon_seamless";
+
+    async function fetchData() {
+      try {
+        const response = await axios.get(currentWeatherURL);
+        setData(response.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    }
+
+    fetchData();
+  }, []);
+
+  return data ? (
     <div className="flex justify-center">
-      <p>Lorem ipsum dolor sit amet</p>
+      <p>{`Aktuell ist es ${data.current.temperature_2m} ${data.current_units.temperature_2m} in ${data.timezone}.`}</p>
     </div>
+  ) : (
+    <p>Keine Daten verfügbar. Überprüfe die Konsole.</p>
   );
 }
